8e1ae2ed4215bb8d50daff586dae87d653c0c9d7
josephtarigan <joseph.tarigan@gmail.com>
2020-05-21 18:24:56 +0700

    Update script

--- PY/proto/TB6612Test_GPIO.py
+++ PY/proto/TB6612Test_GPIO.py
@@ -3,86 +3,109 @@ from time import sleep
 
 # init
 GPIO.setmode(GPIO.BCM)
-MAX_SPEED = 30
+MAX_SPEED = 25
 
 # Standby pin
-PIN_STANDBY = 21
-GPIO.setup(21, GPIO.OUT)
+PIN_STANDBY = 26
+GPIO.setup(PIN_STANDBY, GPIO.OUT)
 
 # Left pins
-PIN_LEFT_PWM = 19
-PIN_LEFT_FORWARD = 5
+PIN_LEFT_FORWARD = 19
 PIN_LEFT_BACKWARD = 6
-GPIO.setup(PIN_LEFT_PWM, GPIO.OUT)
 GPIO.setup(PIN_LEFT_FORWARD, GPIO.OUT)
 GPIO.setup(PIN_LEFT_BACKWARD, GPIO.OUT)
 
 # Right pins
-PIN_RIGHT_PWM = 13
-PIN_RIGHT_FORWARD = 27
+PIN_RIGHT_FORWARD = 13
 PIN_RIGHT_BACKWARD = 17
-GPIO.setup(PIN_RIGHT_PWM, GPIO.OUT)
 GPIO.setup(PIN_RIGHT_FORWARD, GPIO.OUT)
 GPIO.setup(PIN_RIGHT_BACKWARD, GPIO.OUT)
 
 # PWM initialisation
-left_intensity = GPIO.PWM(PIN_LEFT_PWM, 255)
-right_intensity = GPIO.PWM(PIN_RIGHT_PWM, 255)
-left_intensity.start(0)
-right_intensity.start(0)
+left_forward_intensity = GPIO.PWM(PIN_LEFT_FORWARD, MAX_SPEED)
+right_forward_intensity = GPIO.PWM(PIN_RIGHT_FORWARD, MAX_SPEED)
+left_backward_intensity = GPIO.PWM(PIN_LEFT_BACKWARD, MAX_SPEED)
+right_backward_intensity = GPIO.PWM(PIN_RIGHT_BACKWARD, MAX_SPEED)
+left_forward_intensity.start(0)
+right_forward_intensity.start(0)
+left_backward_intensity.start(0)
+right_backward_intensity.start(0)
 
 def stop():
+    left_forward_intensity.ChangeDutyCycle(0)
+    right_forward_intensity.ChangeDutyCycle(0)
+    left_backward_intensity.ChangeDutyCycle(0)
+    right_backward_intensity.ChangeDutyCycle(0)
     GPIO.output(PIN_LEFT_FORWARD, False)
     GPIO.output(PIN_LEFT_BACKWARD, False)
     GPIO.output(PIN_RIGHT_FORWARD, False)
     GPIO.output(PIN_RIGHT_BACKWARD, False)
-    left_intensity.ChangeDutyCycle(0)
-    right_intensity.ChangeDutyCycle(0)
     
-def forward():
-    GPIO.output(PIN_LEFT_FORWARD, True)
-    GPIO.output(PIN_LEFT_BACKWARD, False)
-    GPIO.output(PIN_RIGHT_FORWARD, True)
-    GPIO.output(PIN_RIGHT_BACKWARD, False)
-    left_intensity.ChangeDutyCycle(MAX_SPEED)
-    right_intensity.ChangeDutyCycle(MAX_SPEED)
+def backward(intensity):
+    left_forward_intensity.ChangeDutyCycle(0)
+    right_forward_intensity.ChangeDutyCycle(0)
+    left_backward_intensity.ChangeDutyCycle(intensity)
+    right_backward_intensity.ChangeDutyCycle(intensity)
+    GPIO.output(PIN_LEFT_FORWARD, False)
+    GPIO.output(PIN_LEFT_BACKWARD, True)
+    GPIO.output(PIN_RIGHT_FORWARD, False)
+    GPIO.output(PIN_RIGHT_BACKWARD, True)
     
 def left(intensity):
-    GPIO.output(PIN_LEFT_FORWARD, True)
-    GPIO.output(PIN_LEFT_BACKWARD, False)
+    left_forward_intensity.ChangeDutyCycle(0)
+    right_forward_intensity.ChangeDutyCycle(intensity)
+    left_backward_intensity.ChangeDutyCycle(intensity)
+    right_backward_intensity.ChangeDutyCycle(0)
+    GPIO.output(PIN_LEFT_FORWARD, False)
+    GPIO.output(PIN_LEFT_BACKWARD, True)
     GPIO.output(PIN_RIGHT_FORWARD, True)
     GPIO.output(PIN_RIGHT_BACKWARD, False)
-    left_intensity.ChangeDutyCycle(intensity)
-    right_intensity.ChangeDutyCycle(MAX_SPEED)
     
 def right(intensity):
+    left_forward_intensity.ChangeDutyCycle(intensity)
+    right_forward_intensity.ChangeDutyCycle(0)
+    left_backward_intensity.ChangeDutyCycle(0)
+    right_backward_intensity.ChangeDutyCycle(intensity)
+    GPIO.output(PIN_LEFT_FORWARD, True)
+    GPIO.output(PIN_LEFT_BACKWARD, False)
+    GPIO.output(PIN_RIGHT_FORWARD, False)
+    GPIO.output(PIN_RIGHT_BACKWARD, True)
+
+def forward(intensity):
+    left_forward_intensity.ChangeDutyCycle(intensity)
+    right_forward_intensity.ChangeDutyCycle(intensity)
+    left_backward_intensity.ChangeDutyCycle(0)
+    right_backward_intensity.ChangeDutyCycle(0)
     GPIO.output(PIN_LEFT_FORWARD, True)
     GPIO.output(PIN_LEFT_BACKWARD, False)
     GPIO.output(PIN_RIGHT_FORWARD, True)
     GPIO.output(PIN_RIGHT_BACKWARD, False)
-    left_intensity.ChangeDutyCycle(MAX_SPEED)
-    right_intensity.ChangeDutyCycle(intensity)
-    
+
 def main():
     GPIO.output(PIN_STANDBY, GPIO.HIGH)
-    #print('Forward')
-    #forward()
-    #sleep(5)
-    #stop()
+    print('Right')
+    right(10)
+    sleep(3)
+    stop()
+    sleep(1)
     print('Left')
-    left(20)
-    sleep(3)
-    left(15)
+    left(10)
     sleep(3)
     stop()
-    #print('Right')
-    #right(10)
-    #sleep(5)
+    sleep(1)
+    print('Forward')
+    forward(10)
+    sleep(3)
     stop()
+    sleep(1)
+    print('Backward')
+    backward(10)
+    sleep(3)
+    stop()
+    sleep(1)
     print('Stop')
     
-    left_intensity.stop()
-    right_intensity.stop()
+    
     GPIO.output(PIN_STANDBY, False)
     GPIO.cleanup()
     
